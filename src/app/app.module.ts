574d33a170ff7628b4a6ea8498cc77f5fc7ddf4f
Laerti Papa <laerti.papa@xing.com>
2019-01-26 14:42:28 +0100

    Moves app routing modeule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 import { SharedModule } from "./shared/shared.module";
 
 import { AppComponent } from "./app.component";
